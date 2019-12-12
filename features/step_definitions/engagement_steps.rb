e2f604090ac8c06d2a0340083434d3f893c21a02
Anthony Shao <anthonyshao@berkeley.edu>
2019-12-11 18:06:09 -0800

    attempt to test select2

--- features/step_definitions/engagement_steps.rb
+++ features/step_definitions/engagement_steps.rb
@@ -40,3 +40,20 @@ end
 Then /^I should see "(.*)" is prefilled with number (.+)$/ do |selector, value|
   expect(page).to have_select(selector, :selected => value)
 end
+
+
+When /^I follow Edit for Engagement with id (.+)$/ do |eng_id|
+  engagement = Engagement.find(eng_id)
+  visit edit_app_engagement_path(engagement.app, engagement)
+end
+
+Then /^I select "(.*)" with name "(.+)"$/ do |selector, name|
+  student_id = User.find_by_name(name).id
+  selector.gsub!(/ /, "_")
+  find_field(selector).find("option[value='#{student_id}']").click
+end
+
+Then /^I should see "(.*)" is selected in "(.+)"$/ do |name, selector|
+  student_id = User.find_by_name(name).id
+  expect(page).to have_select(selector, :selected => student_id)
+end
